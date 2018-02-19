9b3f23c00f81957a0173f8c33985ededd03becff
Кирилл Ильин <kirzero2@gmail.com>
2018-02-19 19:48:04 +0600

    доработал модули

---
+++ Application/src/app/modules/employee-module/employee-module.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployeeModuleRoutingModule } from './employee-module-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    EmployeeModuleRoutingModule
+  ],
+  declarations: []
+})
+export class EmployeeModuleModule { }
